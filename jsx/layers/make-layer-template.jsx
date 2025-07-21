4fba69f4a98a4da0cfb14e906eeea05935f9bb1c
masahiro takano <takano@swwwitch.com>
2025-07-21 19:49:45 +0900

    no message

--- jsx/layers/make-layer-template.jsx
+++ jsx/layers/make-layer-template.jsx
@@ -1,5 +1,32 @@
-function act_setLayTmplAttr() {
-  // スクリプトのバージョンとアクションの設定を行う文字列
+/*
+  
+### スクリプト名：
+
+make-layer-template.jsx
+
+### Readme （GitHub）：
+
+https://github.com/swwwitch/illustrator-scripts
+
+### 概要：
+
+- 選択しているレイヤーを「テンプレート」属性（ロック、印刷不可）に変更
+- ダイナミックアクションを使い、ダイアログなしで実行
+
+### 更新履歴：
+
+- v1.0 (20240721) : 初期バージョン
+
+*/
+
+// スクリプトバージョン
+var SCRIPT_VERSION = "v1.0";
+
+function setLayTmplAttr() {
+  /* 
+    アクション文字列の定義
+    Define the action string for setting template layer properties
+  */
   var actionString = 
     '/version 3' +'/name [ 5' +' 6c61796572' +' ]' +'/isOpen 1' +'/actionCount 1' +'/action-1 {' +' /name [ 24' +' 6368616e67652d746f2d74656d706c6174652d6c61796572' +' ]' +' /keyIndex 0' +' /colorIndex 0' +' /isOpen 1' +' /eventCount 1' +' /event-1 {' +' /useRulersIn1stQuadrant 0' +' /internalName (ai_plugin_Layer)' +' /localizedName [ 9' +' e8a1a8e7a4ba203a20' +' ]' +' /isOpen 1' +' /isOn 1' +' /hasDialog 1' +' /showDialog 0' +' /parameterCount 10' +
     ' /parameter-1 {' +' /key 1836411236' +' /showInPalette 4294967295' +' /type (integer)' +' /value 4' +' }' +
@@ -14,18 +41,27 @@ function act_setLayTmplAttr() {
     ' /parameter-10 {' +' /key 1885564532' +' /showInPalette 4294967295' +' /type (unit real)' +' /value 50.0' +' /unit 592474723' +' }' +' }' +
     '}';
 
-  // スクリプトアクションファイルを作成し、アクションを設定
+  /* 
+    アクションファイルを作成して書き込み
+    Create and write temporary action file
+  */
   var actionFile = new File('~/ScriptAction.aia');
   actionFile.open('w');
   actionFile.write(actionString);
   actionFile.close();
 
-  // アクションを読み込み、実行し、削除する
+  /* 
+    アクションを読み込んで実行し、削除
+    Load, execute, and remove the action
+  */
   app.loadAction(actionFile);
   actionFile.remove();
   app.doScript("change-to-template-layer", "layer", false); // action name, set name
   app.unloadAction("layer", ""); // set name
 }
 
-// アクションを設定する関数を実行
-act_setLayTmplAttr();
+/* 
+  メイン処理の実行
+  Run main process
+*/
+setLayTmplAttr();
